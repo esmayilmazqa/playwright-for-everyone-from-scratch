f40b170407c14a60cc91d4178c213f0a5d3e0189
Esma Yılmaz <eeeyilmaz@gmail.com>
2025-12-01 00:21:34 +0300

    first testcases are added

---
+++ tests/UIBasics.spec.js
@@ -0,0 +1,14 @@
+import { test } from '@playwright/test';
+
+test("First testcase with fresh browser and context declaration", async ({browser}) =>
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
+
+test.only("second test without fresh instance", async ({page}) => {
+    await page.goto("https://google.com");
+    // await page.pause(); // browser does not close
+
+});
