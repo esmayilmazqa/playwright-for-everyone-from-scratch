14b720de4aa767190eca14857ff7036cb5652f55
Esma Yılmaz <eeeyilmaz@gmail.com>
2025-12-07 13:19:15 +0300

    select nth index in multiple elements

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,14 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test("First testcase with fresh browser and context declaration", async ({browser}) =>
-{
+test("First testcase with fresh browser and context declaration", async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 });
 
 // test.only
-test("Second test without fresh instance", async ({page}) => {
+test("Second test without fresh instance", async ({ page }) => {
     await page.goto("https://google.com");
     // await page.pause(); // browser does not close
     console.log(await page.title()); // print page title
@@ -16,7 +15,7 @@ test("Second test without fresh instance", async ({page}) => {
 
 });
 
-test.only("Find locator and start ui automation", async({page})=>{
+test("Find locator and start ui automation on negative login scenario", async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title()); // LoginPage Practise | Rahul Shetty Academy
     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
@@ -26,8 +25,25 @@ test.only("Find locator and start ui automation", async({page})=>{
     await page.locator('#signInBtn').click();
     // wait until this block locator shown up in the page
     console.log(await page.locator("[style*='block']").textContent()); // Incorrect username/password.
-    await expect(page.locator("[style*='block']")).toContainText("Incorrect");     // alternative full text : await expect(locator).toHaveText()
-    
-    
-   // await page.pause();
+    await expect(page.locator("[style*='block']")).toContainText("Incorrectx");     // alternative full text : await expect(locator).toHaveText()
+
+
+    // await page.pause();
+});
+
+test("Select nth index in multiple elements", async ({page}) =>{
+    const txtUsername = page.locator("#username");
+    const btnSignIn = page.locator('#signInBtn');
+    await  page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+    await txtUsername.fill("username ");
+    await page.locator("#password").fill("learning");
+    await txtUsername.fill("rahulshettyacademy"); // yazmadan once temizliyor
+    await btnSignIn.click();
+    console.log(await page.locator("div.card-body a").first().textContent());
+    console.log(await page.locator("div.card-body a").nth(1).textContent());
+    console.log(await page.locator("div.card-body a").last().textContent());
+
+
+    //await page.pause();
 });
