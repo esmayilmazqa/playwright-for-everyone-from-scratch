d7b0d4e362daa4314129bd6a758a489334ffbc1f
Esma Yılmaz <eeeyilmaz@gmail.com>
2026-02-08 16:04:44 +0300

    login screen is bypassed by using login API

---
+++ tests/WebAPIentegration.spec.js
@@ -0,0 +1,128 @@
+import { test, expect, request } from "@playwright/test";
+/**
+ * UIE2E.spec.js scenario with Login API Injection
+ */
+const loginPayload = { userEmail: "academy123+@gmail.com", userPassword: "Academy123+" };
+let token;
+
+test.beforeAll(async () => {
+    // Login with API and generated token from response
+    const apiContext = await request.newContext();
+    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
+        {
+            data: loginPayload
+        }
+    );
+    expect(loginResponse.ok()).toBeTruthy();
+    const responseJson = await loginResponse.json();
+    token = responseJson.token;
+    console.log("Token : ", token);
+});
+
+test.beforeEach(async () => {
+
+});
+
+
+
+test("Send a request and parse the response", async (page) => {
+
+});
+
+test.only("E2E scenario", async ({ page }) => {
+
+    // token from login API and be set on localStorage
+    page.addInitScript(value => {
+        window.localStorage.setItem('token', value);
+    }, token);
+
+    await page.goto("https://rahulshettyacademy.com/client/");
+
+
+    const productName = "iphone 13 pro";
+    const products = page.locator("div.card-body"); // this is array
+    const txtUsername = page.locator("input#userEmail");
+    const txtPassword = page.locator("input#userPassword");
+    const lblFilters = page.locator("h4").nth(1);
+    const email = 'academy123+@gmail.com';
+
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+
+
+
+    let count = await products.count(); // array lentgh
+    for (let i = 0; i < count; i++) {
+
+        const title = await products.nth(i).locator("b").textContent(); // chaining locator
+
+        if (title && title.includes("iphone")) { // title problem is solved as added null control
+            console.log("title : ", title);
+            console.log("Found : ", i, " index");
+            await products.nth(i).locator("text= Add To Cart").click();
+            // alternative 1 : await products.nth(i).locator(':has-text("Add To Cart")').click();
+            // alternative 2: await products.nth(i).locator('button', { hasText: 'Add To Cart' }).click();
+
+            break;
+        }
+    }
+
+    await page.locator("button[routerlink*='cart']").click();
+
+    // alternative  // await expect(page.locator(`h3:has-text("${productName}")`)).toBeVisible();
+    await page.locator("div.cart li").waitFor();
+    const isPresent = await page.locator(`h3:has-text("${productName}")`).isVisible();
+
+    expect(isPresent).toBeTruthy();
+
+    // check checkout elements
+    await page.locator("ul button[type='button']").click();
+
+    await page.locator("input[placeholder*='Country']").pressSequentially("ind");
+
+    const cbbCountryPanel = page.locator("section.ta-results");
+    await cbbCountryPanel.waitFor();
+
+    const optionCount = await cbbCountryPanel.locator("button").count();
+    for (let i = 0; i < optionCount; i++) {
+        const text = await cbbCountryPanel.locator("button").nth(i).textContent();
+        // console.log(text);
+        if (text && text.trim() === "India") // solved text error with null control
+        {
+            cbbCountryPanel.locator("button").nth(i).click();
+        }
+    }
+
+    await expect(page.locator("div.user__name label[type='text']")).toHaveText(email);
+    await page.locator("a.action__submit").click();
+    await expect(page.locator("h1.hero-primary")).toHaveText(" Thankyou for the order. ");
+
+    const orderId = await page.locator("td.em-spacer-1 label.ng-star-inserted").textContent();
+    console.log(orderId);
+
+    await page.locator("button[routerLink*='orders']").click();
+
+    const rows = page.locator("tbody tr");
+    await rows.nth(0).waitFor();
+    count = await rows.count();
+
+    for (let i = 0; i < count; i++) {
+        const rowOrderId = await rows.nth(i).locator("th").textContent();
+        if (orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+
+    }
+
+    const lblOrder = page.locator("div.col-text.-main");
+    console.log("orderId ", orderId); //  | 695963e0c941646b7a7b4fca |
+    // await lblOrder.waitFor(); 
+    console.log("lblOrderText : ", await lblOrder.textContent());
+    expect(lblOrder).toHaveText(orderId?.replaceAll("|", "").replaceAll(" ", ""));
+
+
+
+    await page.pause();
+
+
+});
