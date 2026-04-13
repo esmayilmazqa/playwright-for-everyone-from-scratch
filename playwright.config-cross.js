906bbe671f423218cd7b44c18e9d723f9a3fed83
Esma Yılmaz <eeeyilmaz@gmail.com>
2026-04-14 00:29:52 +0300

    playwright cross config file is added.

---
+++ playwright.config-cross.js
@@ -0,0 +1,62 @@
+// npx playwright test tests/xx.spec.js --config playwright.config.js --project=chromium
+import { chromium, defineConfig, devices } from '@playwright/test';
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,  // for components
+  expect: {
+    timeout: 5000, // for assertions
+
+  },
+
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        browserName: 'chromium',
+        headless: false,
+        slowMo: 0,
+        screenshot: 'only-on-failure', // only-on-failure, on-first-failure, on
+        trace: 'retain-on-failure', // retry-with-trace, on-first-retry, on-all-retries, on
+        video: 'retain-on-failure',
+
+
+        launchOptions: {
+          args: ['--disable-dev-shm-usage'],
+        },
+      },
+    },
+    {
+      name: 'chromium-headless',
+      use: {
+        browserName: 'chromium',
+        headless: true,
+        slowMo: 0,
+        screenshot: 'only-on-failure', // only-on-failure, on-first-failure, on
+        trace: 'retain-on-failure', // retry-with-trace, on-first-retry, on-all-retries, on
+        video: 'retain-on-failure',
+
+        launchOptions: {
+          args: ['--disable-dev-shm-usage'],
+        },
+      },
+    }
+  ],
+
+
+});
+
--- tests/UIE2EaPageObjectDataFixture.spec.js
+++ tests/UIE2EaPageObjectDataFixture.spec.js
@@ -1,49 +1,50 @@
-import {customtest, expect} from "../test-factory/base-test.js";
+import { customtest, expect } from "../test-factory/base-test.js";
 import { PageObjectManager } from '../page-objects/page-object-manager';
 
+customtest(`E-commerce PO automation with traditional locators and customtest by fixture`, async ({ page, testDataForOrder }) => {
 
-  customtest.only(`E-commerce PO automation with traditional locators and customtest by fixture`, async ({ page, testDataForOrder }) => {
+  const pomManager = new PageObjectManager(page, testDataForOrder.productName);
+  await pomManager.getLoginPage().landOnPage();
+  await pomManager.getLoginPage().validLogin(testDataForOrder.username, testDataForOrder.password);
+  // await page.waitForLoadState("networkidle"); // not enought, put some control mechanism (crashed)
+  await expect(pomManager.getLoginPage().lblFilters).toHaveText("Filters"); // auto-wait working
+  // console.log("text : ", await dashboardPage.lblFilters.textContent());
 
-    const pomManager = new PageObjectManager(page, testDataForOrder.productName);
-    await pomManager.getLoginPage().landOnPage();
-    await pomManager.getLoginPage().validLogin(testDataForOrder.username, testDataForOrder.password);
-    // await page.waitForLoadState("networkidle"); // not enought, put some control mechanism (crashed)
-    await expect(pomManager.getLoginPage().lblFilters).toHaveText("Filters"); // auto-wait working
-    // console.log("text : ", await dashboardPage.lblFilters.textContent());
+  await pomManager.getDashboardPage().selectAndAddToCart(testDataForOrder.productName);
+  await pomManager.getDashboardPage().clickCartMenu();
 
-    await pomManager.getDashboardPage().selectAndAddToCart(testDataForOrder.productName);
-    await pomManager.getDashboardPage().clickCartMenu();
+  await expect(pomManager.getCartPage().productNameInCart.isVisible()).toBeTruthy();
 
-    await expect(pomManager.getCartPage().productNameInCart.isVisible()).toBeTruthy();
+  // check checkout elements
+  await pomManager.getCartPage().clickCheckoutBtn();
+  await pomManager.getCheckoutPage().pressSequentiallyCountry("ind");
+  await pomManager.getCheckoutPage().selectCountry("India");
 
-    // check checkout elements
-    await pomManager.getCartPage().clickCheckoutBtn();
-    await pomManager.getCheckoutPage().pressSequentiallyCountry("ind");
-    await pomManager.getCheckoutPage().selectCountry("India");
+  await expect(pomManager.getCheckoutPage().lblUsername).toHaveText(testDataForOrder.username);
 
-    await expect(pomManager.getCheckoutPage().lblUsername).toHaveText(testDataForOrder.username);
-
-    await pomManager.getCheckoutPage().clickPlaceOrderBtn();
-
-
-    // order details page
-    await expect(await pomManager.getOrderDetailsPage().msgThankYou).toHaveText(" Thankyou for the order. ");
-
-    const orderId = await pomManager.getOrderDetailsPage().orderId.textContent();
-    // await page.locator("td.em-spacer-1 label.ng-star-inserted").textContent();
-    console.log(orderId);
+  await pomManager.getCheckoutPage().clickPlaceOrderBtn();
 
 
-    // OrdersPage 
-    await pomManager.getDashboardPage().clickOrdersMenu();
-    await pomManager.getOrdersPage().clickViewBtnForAddedOrder(orderId);
+  // order details page
+  await expect(await pomManager.getOrderDetailsPage().msgThankYou).toHaveText(" Thankyou for the order. ");
+
+  const orderId = await pomManager.getOrderDetailsPage().orderId.textContent();
+  // await page.locator("td.em-spacer-1 label.ng-star-inserted").textContent();
+  console.log(orderId);
 
 
-    // Order Summary
-    const lblOrder = await pomManager.getOrderDetailsPage().getOrderId();
-    expect(lblOrder).toHaveText(orderId?.replaceAll("|", "").replaceAll(" ", ""));
+  // OrdersPage 
+  await pomManager.getDashboardPage().clickOrdersMenu();
+  await pomManager.getOrdersPage().clickViewBtnForAddedOrder(orderId);
 
 
-    await page.pause();
+  // Order Summary
+  const lblOrder = await pomManager.getOrderDetailsPage().getOrderId();
+  expect(lblOrder).toHaveText(orderId?.replaceAll("|", "").replaceAll(" ", ""));
 
-  });
+
+  await page.pause();
+
+});
+
+
